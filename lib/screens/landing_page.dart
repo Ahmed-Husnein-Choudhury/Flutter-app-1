0345f846e494596d85de1f07ee8cefb9e464ab9d
Ahmed-Husnein-Choudhury <ahusnein@iquantile.com>
2018-12-26 16:05:13 +0600

    user existence is checked before logging in.

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:medicaid/screens/voice_login.dart';
 import 'package:medicaid/screens/member_registration.dart';
 import 'package:medicaid/utils/common_widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:medicaid/screens/voice_registration_set_up.dart';
 
 class LandingPage extends StatefulWidget {
-
   // defining the route here
   static final routeName = "/landingPage";
 
@@ -14,22 +15,22 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
-
   // defining the login button widget
+  bool isRegistered=false;
+
   Widget loginButton() {
+    checkIfUserRegistered();
+
     return Container(
       height: 50.0,
       width: 250.0,
       child: RaisedButton(
         color: Color(0XFF00AFDF),
         //onPressed: () => Navigator.of(context).pushNamedAndRemoveUntil(HomePage.routeName, ModalRoute.withName(HomePage.routeName)),
-        onPressed: () => Navigator.of(context).pushNamed(VoiceLogin.routeName),
+        onPressed: () => isRegistered? Navigator.of(context).pushNamed(VoiceLogin.routeName):showErrorDialog(),
         child: Text(
           "Login",
-          style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.white
-          ),
+          style: TextStyle(fontSize: 18.0, color: Colors.white),
         ),
         shape: StadiumBorder(
           side: BorderSide(
@@ -48,13 +49,12 @@ class _LandingPageState extends State<LandingPage> {
       width: 250.0,
       child: RaisedButton(
         color: Color(0XFF1EE3B7),
-        onPressed: () => Navigator.of(context).pushNamed(MemberRegistration.routeName),
+        onPressed: () =>
+            Navigator.of(context).pushNamed(MemberRegistration.routeName),
+          //  Navigator.of(context).pushNamed(VoiceRegistrationSetUp.routeName),
         child: Text(
           "Register",
-          style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.white
-          ),
+          style: TextStyle(fontSize: 18.0, color: Colors.white),
         ),
         shape: StadiumBorder(
           side: BorderSide(
@@ -70,52 +70,109 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                //logo(),
-                CommonWidgets.spacer(gapHeight: 20.0),
-                Text(
-                  "Welcome!",
-                  style: TextStyle(
-                    color: Color(0XFF00AFDF),
-                    fontSize: 34.0,
-                  ),
-                ),
-                CommonWidgets.spacer(gapHeight: 15.0),
-                Text(
-                  "Please log in to continue.",
-                  style: TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.grey
-                  ),
-                ),
-                CommonWidgets.spacer(gapHeight: 10.0),
-                Text(
-                  "Don't have any account?",
-                  style: TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.grey
-                  ),
-                ),
-                Text(
-                  "Please register for one.",
-                  style: TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.grey
-                  ),
-                ),
-                CommonWidgets.spacer(gapHeight: 80.0),
-                loginButton(),
-                CommonWidgets.spacer(gapHeight: 25.0),
-                registerButton(),
-                CommonWidgets.spacer(gapHeight: 20.0),
-              ],
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //logo(),
+            CommonWidgets.spacer(gapHeight: 20.0),
+            Text(
+              "Welcome!",
+              style: TextStyle(
+                color: Color(0XFF00AFDF),
+                fontSize: 34.0,
+              ),
             ),
-          ),
-        )
+            CommonWidgets.spacer(gapHeight: 15.0),
+            Text(
+              "Please log in to continue.",
+              style: TextStyle(fontSize: 14.0, color: Colors.grey),
+            ),
+            CommonWidgets.spacer(gapHeight: 10.0),
+            Text(
+              "Don't have any account?",
+              style: TextStyle(fontSize: 14.0, color: Colors.grey),
+            ),
+            Text(
+              "Please register for one.",
+              style: TextStyle(fontSize: 14.0, color: Colors.grey),
+            ),
+            CommonWidgets.spacer(gapHeight: 80.0),
+            loginButton(),
+            CommonWidgets.spacer(gapHeight: 25.0),
+            registerButton(),
+            CommonWidgets.spacer(gapHeight: 20.0),
+          ],
+        ),
+      ),
+    ));
+  }
+
+  void checkIfUserRegistered() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if(    prefs.getBool("is registered") != null &&   prefs.getBool("is registered") !=false){
+      isRegistered=true;
+    }
+  }
+
+  void showErrorDialog() {
+      showDialog(context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              title: Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "You haven't registered yet!!",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 7.0),
+                    ),
+                    Divider(
+                     height: 2.0,
+
+                    )
+                  ],
+                ),
+              ),
+              content: Container(
+                height: 100.0,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "Please register before logging in.",
+
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 15.0),
+                    ),
+                    CommonWidgets.spacer(gapHeight: 25.0),
+                    RaisedButton(
+                        color: Color(0XFF00AFDF),
+                        shape: StadiumBorder(
+                          side: BorderSide(
+                            width: 1.0,
+                            color: Color(0XFF00AFDF),
+                          ),
+                        ),
+                        child: Text(
+                            "OK",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0
+                            )
+                        ),
+                        onPressed:
+                        ()=>Navigator.of(context).pop()
+
+                    )
+                  ],
+                ),
+              ));
+        },
     );
   }
 }
--- lib/screens/voice_registration_set_up.dart
+++ lib/screens/voice_registration_set_up.dart
@@ -7,19 +7,19 @@ import 'package:medicaid/utils/common_widgets.dart';
 import 'package:simple_permissions/simple_permissions.dart';
 import 'package:medicaid/screens/member_registration.dart';
 import 'voice_login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class VoiceRegistrationSetUp extends StatefulWidget {
-
-  static final String routeName="/voiceRegistrationSetup";
+  static final String routeName = "/voiceRegistrationSetup";
 
   @override
   _State createState() => _State();
 }
 
-const _voiceRecognitionMethodChannel = const MethodChannel("biometric authentication");
+const _voiceRecognitionMethodChannel =
+    const MethodChannel("biometric authentication");
 
 class _State extends State<VoiceRegistrationSetUp> {
-
   Widget logo() {
     return Center(
       child: Image.asset(
@@ -32,49 +32,37 @@ class _State extends State<VoiceRegistrationSetUp> {
   Widget instructionalText(String text) {
     return Text(
       text,
-
     );
   }
 
   Widget continueButton() {
     return Padding(
-      padding: EdgeInsets.fromLTRB(40.0, 16.0, 40.0, 0.0),
-      child: Container(
-        height: 45,
-      width: 200,
-      child:RaisedButton(
-          color: Color(0XFF00AFDF),
-          shape: StadiumBorder(
-            side: BorderSide(
-              width: 0.5,
-              color: Color(0XFF00AFDF),
-            ),
-          ),
-          child: Text(
-              "Continue",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15.0
-              )
-          ),
-          onPressed: requestPermission
-
-      )
-      )
-    );
+        padding: EdgeInsets.fromLTRB(40.0, 16.0, 40.0, 0.0),
+        child: Container(
+            height: 45,
+            width: 200,
+            child: RaisedButton(
+                color: Color(0XFF00AFDF),
+                shape: StadiumBorder(
+                  side: BorderSide(
+                    width: 0.5,
+                    color: Color(0XFF00AFDF),
+                  ),
+                ),
+                child: Text("Continue",
+                    style: TextStyle(color: Colors.white, fontSize: 15.0)),
+                onPressed: requestPermission)));
   }
 
   requestPermission() async {
-    final res = await SimplePermissions.requestPermission(Permission.RecordAudio);
-    if (res==PermissionStatus.authorized) {
+    final res =
+        await SimplePermissions.requestPermission(Permission.RecordAudio);
+    if (res == PermissionStatus.authorized) {
       print("audio permission granted:$res");
       final req = await SimplePermissions.requestPermission(
           Permission.WriteExternalStorage);
-      if (req==PermissionStatus.authorized) {
-
-        setState(() {
-
-        });
+      if (req == PermissionStatus.authorized) {
+        setState(() {});
 
         _registerWithVoice();
       }
@@ -82,43 +70,51 @@ class _State extends State<VoiceRegistrationSetUp> {
   }
 
   Future<Null> _registerWithVoice() async {
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
     bool response;
-    response = await _voiceRecognitionMethodChannel.invokeMethod("register voice",{"name":fullName});
+    response = await _voiceRecognitionMethodChannel
+        .invokeMethod("register voice", {"name": fullName});
     print("native is being called:$response");
-    response ? Navigator.of(context).pushNamed(VoiceLogin.routeName) : "not working";
+
+    prefs.setBool("is registered", response);
+
+    response
+        ? Navigator.of(context).pushNamed(VoiceLogin.routeName)
+        : "not working";
   }
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
         onWillPop: exitApp,
-      child: Scaffold(
-        body: SingleChildScrollView(
+        child: Scaffold(
+            body: SingleChildScrollView(
           padding: EdgeInsets.all(20.0),
-      child: Container(
-        child:Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            CommonWidgets.spacer(gapHeight: 20.0),
-            CommonWidgets.logo(),
-            CommonWidgets.spacer(gapHeight: 30.0),
-            Text(
-              "Almost Done! Let's add your second one.",
-              textAlign: TextAlign.start,
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                CommonWidgets.spacer(gapHeight: 20.0),
+                CommonWidgets.logo(),
+                CommonWidgets.spacer(gapHeight: 30.0),
+                Text(
+                  "Almost Done! Let's add your second one.",
+                  textAlign: TextAlign.start,
+                ),
+                CommonWidgets.spacer(gapHeight: 20.0),
+                instructionalText(
+                    "To register your voice, please allow access to your device's microphone or contact your health plan."),
+                CommonWidgets.spacer(gapHeight: 30.0),
+                continueButton(),
+                CommonWidgets.spacer(gapHeight: 30.0),
+                instructionalText("Health Plan Service 1"),
+                instructionalText("Customer Service (800) 555-2222")
+              ],
             ),
-            CommonWidgets.spacer(gapHeight: 20.0),
-            instructionalText("To register your voice, please allow access to your device's microphone or contact your health plan."),
-            CommonWidgets.spacer(gapHeight: 30.0),
-            continueButton(),
-            CommonWidgets.spacer(gapHeight: 30.0),
-          instructionalText("Health Plan Service 1"),
-          instructionalText("Customer Service (800) 555-2222")
-          ],
-        ),
-      ),
-    ))
-
-    );
+          ),
+        )));
   }
 
   Future<bool> exitApp() {

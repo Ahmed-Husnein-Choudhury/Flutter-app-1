62a02343aed75268bf16de01781c53ba4c18b5eb
Ahmed-Husnein-Choudhury <ahusnein@iquantile.com>
2019-01-08 10:40:50 +0600

    lots of design changes made and improved.

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,8 @@ Future<void> main() async {
       title: "B.Hold",
       debugShowCheckedModeBanner: false,
       routes: Routes.routeList,
-     // home:FacialRecognitionSetup(healthPlanName: "Square",)
-      home:LandingPage()
+      home:FacialRecognitionSetup(healthPlanName: "Square",)
+      //home:LandingPage()
   ));
 }
 //g

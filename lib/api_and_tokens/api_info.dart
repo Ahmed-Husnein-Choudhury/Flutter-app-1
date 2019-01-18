35dc54a0b628b6245f9febc623a7c747949086ef
Ahmed-Husnein-Choudhury <ahusnein@iquantile.com>
2019-01-18 16:20:30 +0600

    Phase 2 Started

---
+++ lib/api_and_tokens/api_info.dart
@@ -0,0 +1,10 @@
+class ApiInfo {
+
+   static const String _baseUrl = "http://ec2-3-83-176-152.compute-1.amazonaws.com:8008";
+
+   static String getBaseUrl()
+   {
+      return _baseUrl;
+   }
+
+}
--- lib/screens/member_information.dart
+++ lib/screens/member_information.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
 
-import 'package:medicaid/member.dart';
+import 'package:medicaid/models/member.dart';
 import 'package:medicaid/screens/facial_setup.dart';
 import 'package:medicaid/screens/voice_registration_set_up.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
 
 class MemberInformation extends StatefulWidget {
@@ -35,12 +36,17 @@ class _MemberInformationState extends State<MemberInformation> {
     super.initState();
     this.member = Member.fromJson(json.decode(widget.responseData));
     phoneNumber=this.member.contactInfo.primaryPhoneNumber;
-
-
+    saveMemberNumber();
 //    while(numberFormatCounter<=2){
 //     subString.add(splitString(phoneNumber,0,2));
 //
 //    }
+  }
+
+  Future<Null> saveMemberNumber() async{
+    SharedPreferences memberIdPref=await SharedPreferences.getInstance();
+    memberIdPref.setString("member number", this.member.memberNumber);
+    print("member number saved:${this.member.memberNumber}");
   }
 
  String splitString(String inputString,int startIndex,int endIndex){
@@ -302,7 +308,6 @@ class _MemberInformationState extends State<MemberInformation> {
 
   @override
   Widget build(BuildContext context) {
-
 
     return Scaffold(
       body: SingleChildScrollView(
